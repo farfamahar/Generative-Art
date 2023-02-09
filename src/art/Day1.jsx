b93c7c622238ae04190d0d58a479b0ec1e920ac2
Farfama <farfama.hargaaya@gmail.com>
2023-02-09 23:44:20 +0900

    Add simplex noise

--- src/art/Day1.jsx
+++ src/art/Day1.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Sketch from "react-p5";
 
-	let x = 50;
-	let y = 50;
+let x = 50;
+let y = 50;
 export default (props) => {
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
