fb969ef61d4d32ea2353157b79ae34b627bc5fdf
Farfama <farfama.hargaaya@gmail.com>
2023-02-09 23:36:32 +0900

    Add, vite, p5-js setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Day1 from './art/Day1'
+
+function App() {
+
+  return (
+    <div>
+      <Day1/>
+    </div>
+  )
+}
+
+export default App
