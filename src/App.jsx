e0a25ba3e082b8270d1ea3b6bfc0d5ddcc77f451
Farfama <farfama.hargaaya@gmail.com>
2023-02-14 23:14:42 +0900

    Add rectangle rotations, color

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Day1 from './art/Day1'
 import Day2 from './art/Day2'
+import Day3 from './art/Day3'
 
 function App() {
 
   return (
     <div className="flex h-screen justify-center items-center" >
       {/* <Day1 className="m-auto"/> */}
-      <Day2 className="m-auto"/>
+      <Day3 className="m-auto"/>
     </div>
   )
 }
