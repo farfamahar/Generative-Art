95e94dac00f85bbae1a7b657d85c47dc97a1e0f3
Farfama <farfama.hargaaya@gmail.com>
2023-02-15 14:12:20 +0900

    Update point count, color alpha

--- src/art/Day4.jsx
+++ src/art/Day4.jsx
@@ -14,11 +14,11 @@ export default (props) => {
         p5.noStroke()
 
         // create points randomly on canvas
-        for(let i = 0; i < 10; i=i+1){
+        for(let i = 0; i < 100; i=i+1){
             let x = p5.random(canvasWidth);
             let y = p5.random(canvasHeight);
             let a = p5.random(p5.TWO_PI); //angle
-            let fill  = p5.color(p5.random(255), p5.random(255), p5.random(255), 50);
+            let fill  = p5.color(p5.random(255), p5.random(255), p5.random(255));
             points[i] = {
                 x:x,
                 y:y,
@@ -32,7 +32,9 @@ export default (props) => {
     }
 
     function draw(p5){
-        for(let i = 0; i < 10; i+=1){
+        // p5.background(30);
+
+        for(let i = 0; i < 100; i+=1){
             let point = points[i];
             point.x = point.x + point.dx
             point.y = point.y + point.dy
@@ -41,10 +43,10 @@ export default (props) => {
                 point.dx = point.dx * -1;
             }
             if(point.y>canvasHeight || point.y <0){
-                point.dy = point.dxy * -1;
+                point.dy = point.dy * -1;
             }
             p5.fill(point.fill);
-            p5.circle(point.x,point.y,50);
+            p5.circle(point.x,point.y,10);
 
         }
     }
