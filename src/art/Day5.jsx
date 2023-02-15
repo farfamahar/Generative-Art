d1b61faa28b2ed29e8c1253c7b85b108f4d40ab5
Farfama <farfama.hargaaya@gmail.com>
2023-02-16 05:26:48 +0900

    Update Day5.jsx

--- src/art/Day5.jsx
+++ src/art/Day5.jsx
@@ -10,13 +10,13 @@ export default (props) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
-        for (let i = 0; i < 100; i++) {
-            let x = p5.random(canvasWidth);
-            let y = p5.random(canvasHeight);
-            let angle = 0;
-            let amplitude = p5.random(10, 20);
-            let period = p5.random(50, 100);
-            blades.push({x, y, angle, amplitude, period});
+        for (let i = 0; i < 500; i++) {
+            let x = p5.random(-500,500);
+            let y = 50;
+            let angle = p5.random(10,20);
+            let amplitude = 0.1;
+            let period = p5.random(0, 500);
+            blades.push({x, y, angle, amplitude, period})
           }
     
 
@@ -26,15 +26,15 @@ export default (props) => {
     function draw(p5){
         // p5.background(30);
 
-        p5.background(220);
+        p5.background(30);
         for (let blade of blades) {
           blade.angle += 0.02;
           p5.push();
           p5.translate(blade.x, blade.y);
           p5.rotate(p5.sin(blade.angle) * blade.amplitude);
-          p5.stroke(0, 128, 0);
-          p5.strokeWeight(2);
-          p5.line(0, 0, 0, -50);
+          p5.stroke('white');
+          p5.strokeWeight(1);
+          p5.line(1000, 1000, 0, -50);
           p5.pop();
         }
 
