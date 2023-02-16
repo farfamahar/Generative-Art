5f830b6b772ba6051582828a77a4fc33519d71e9
Farfama <farfama.hargaaya@gmail.com>
2023-02-16 14:48:09 +0900

    Update Day6.jsx

--- src/art/Day6.jsx
+++ src/art/Day6.jsx
@@ -3,13 +3,18 @@ import Sketch from "react-p5";
 
 const canvasWidth = 500;
 const canvasHeight = 500;
-let blades = [];
+let circles = {};
+let r;
+let g;
+let b;
 
 export default (props) => {
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
+
         p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
+  
 
 
     
@@ -21,10 +26,18 @@ export default (props) => {
         p5.background('black');
         p5.noFill(); // set the fill color to transparent
         p5.stroke(255); // set the stroke color to white
-        p5.strokeWeight(1.5); // set the stroke weight to 4 pixels 
+        p5.strokeWeight(4); // set the stroke weight to 4 pixels
+        r = p5.random(255); // r is a random number between 0 - 255
+        g = p5.random(255); // g is a random number betwen 100 - 200
+        b = p5.random(255); // b is a random number between 0 - 100
+        // p5.frameRate(15)
         // draw a series of concentric circles
         for (let i = 0; i < canvasWidth*2; i += 20) {
-          p5.ellipse(canvasHeight/2, canvasHeight/2, i+=2);
+            let alpha = p5.map(i, 0, canvasWidth/0.5, 255, 0); // calculate the alpha value based on the distance from the center
+            p5.stroke(r,g,b,alpha); // set the stroke color with alpha value
+            p5.ellipse(canvasHeight/2, canvasHeight/2, i+=2);
+            p5.strokeWeight(p5.random(20)); // set the stroke weight to 4 pixels
+
         }   
         // draw a black circle at the center of the canvas
         p5.fill(0); // set the fill color to black
