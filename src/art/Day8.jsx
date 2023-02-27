cdcd8e6599ea1b0fe76d97cf0692770ed3be75b8
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 18:09:34 +0900

    Create Day8.jsx

---
+++ src/art/Day8.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Sketch from "react-p5";
+
+const canvasWidth = 500;
+const canvasHeight = 500;
+
+export default (props) => {
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+
+    p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
+
+  };
+
+  function draw(p5) {
+
+
+  }
+
+  return <Sketch setup={setup} draw={draw} className="" />;
+};
