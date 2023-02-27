5c759bf8ebb6a4a4f5dc0cc2df571da88cd5a47e
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 18:30:43 +0900

    Update waves created

--- src/art/Day8.jsx
+++ src/art/Day8.jsx
@@ -21,15 +21,16 @@ export default (props) => {
     p5.noFill();
     p5.stroke(255);
 
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < 20; i++){
         p5.beginShape();
 
         for(let j = 0; j < 360; j+= 10){
             let rad = i * 8;
             let x = rad * p5.cos(j);
             let y = rad * p5.sin(j);
+            let z = p5.sin(p5.frameCount + i * 10) * 50;
 
-            p5.vertex(x, y,);
+            p5.vertex(x, y, z);
         }
 
         p5.endShape(p5.CLOSE);
