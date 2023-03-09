527a74aa7afa1d31b5b08cf088bc8ef0066c3e13
Farfama <farfama.hargaaya@gmail.com>
2023-03-09 14:45:18 +0900

    Update randomness

--- src/art/Day9.jsx
+++ src/art/Day9.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Sketch from "react-p5";
 import { CANVAS_WIDTH, CANVAS_HEIGHT } from "/src/data/const";
 let start = 0;
+let random1;
+let random2;
 
 export default (props) => {
   const setup = (p5, canvasParentRef) => {
@@ -14,6 +16,9 @@ export default (props) => {
 
     p5.angleMode(p5.DEGREES);
     p5.noiseDetail(2, 1);
+    random1 = p5.random(1,5)
+    random2 = p5.random(0.1,0.5)
+
   };
 
   function draw(p5) {
@@ -21,17 +26,23 @@ export default (props) => {
     p5.noStroke();
     p5.translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
 
-    let space = 1; // control how much i is increased each loop ()
+    let space = random2; // lower = less space between lines
 
     for (let i = 0; i < 360; i += space) {
       // perlin noise to control the height
-      let xoff = p5.map(p5.cos(i), -1, 1, 0, 3);
-      let yoff = p5.map(p5.sin(i), -1, 1, 0, 3);
+      let xoff = p5.map(p5.cos(i*random1), -1, random1, 0, random1);
+      let yoff = p5.map(p5.sin(i*random1), -1, random1, 0, random1);
 
       let noise = p5.noise(xoff + start, yoff + start);
       let height = p5.map(noise, 0, 1, -150, 150);
 
+      //colors
+      let r = p5.map(p5.sin(i), -1, 1, 100, 200);
+      let g = p5.map(height, -150, 150, 0, 150);
+      let b = p5.map(noise, 0, 1, 150, 255);
+
       p5.rotate(space);
+      p5.fill(r, g, b);
       p5.rect(200, 0, height, 1);
     }
     start +=0.01;
