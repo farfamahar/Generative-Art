d6b9a383f4f178e2abe1bf95a4a67827be06961b
Farfama <farfama.hargaaya@gmail.com>
2023-03-23 14:34:37 +0900

    Create Day10.jsx
    Initial commit

---
+++ src/art/Day10.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Sketch from "react-p5";
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from "/src/data/const";
+
+
+export default (props) => {
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+
+    p5.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT).parent(
+      canvasParentRef
+    );
+
+  };
+
+  function draw(p5) {
+  }
+
+  return <Sketch setup={setup} draw={draw} className="" />;
+};
